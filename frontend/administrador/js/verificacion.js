270534dfaeab551c72279ab4cd11343fcefa69b8
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2026-05-12 19:20:11 -0500

    html

--- frontend/administrador/js/verificacion.js
+++ frontend/administrador/js/verificacion.js
@@ -1,14 +1,14 @@
 async function nombre() {
     const usuario = JSON.parse(localStorage.getItem("usuario"));
     
-    nombre = document.getElementById("bienvenido")
+    nombre = document.getElementById("nombre")
     
     if (!usuario) {
 
     window.location.href = "../../ingreso/ingreso.html";
 
     } else{
-        nombre.innerText = "Hola " + usuario.nombre + " bienvenido al sistema de biblioteca de prueba"
+        nombre.innerText = "Hola " + usuario.nombre 
     }
 }
 
